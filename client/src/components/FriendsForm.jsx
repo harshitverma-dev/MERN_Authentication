aaf58a81e586e79769247e4a822f61f1f7fe1500
harshitverma-dev <theharshitverma53@gmail.com>
2023-06-10 11:41:29 +0530

    added form  button disabled functionality

--- client/src/components/FriendsForm.jsx
+++ client/src/components/FriendsForm.jsx
@@ -117,10 +117,7 @@ const FriendsForm = () => {
                             placeholder="Enter location"
                             onChange={onChangeFun} />
                     </Form.Group>
-                    <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                        <Form.Check type="checkbox" label="Check me out" />
-                    </Form.Group>
-                    <Button variant="primary" type="submit" onClick={submitFriendsData}>
+                    <Button onLoad={true} variant="primary" type="submit" onClick={submitFriendsData} disabled={friendsData.firstName && friendsData.lastName && friendsData.friendEmail && friendsData.jobRole && friendsData.location ? false : true}>
                         Submit
                     </Button>
                 </Form>
